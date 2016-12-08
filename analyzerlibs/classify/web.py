eab027e350e3266bf5ec7cc0cc4ed632c4e9981d
dark.H <darkhackdevil@gmail.com>
2016-12-08 21:50:53 +0800

    add special

--- analyzerlibs/classify/web.py
+++ analyzerlibs/classify/web.py
@@ -3,7 +3,6 @@ import jieba.analyse
 from bs4 import BeautifulSoup
 from analyzerlibs.wordslib import stat
 
-
 def init(parallel=1):
     jieba.initialize()
 
@@ -13,6 +12,8 @@ class WebSta:
         self.keys_num = num
         self._res = ana_instance
         self.text = ana_instance.text()
+        self.css = len(ana_instance.css)
+        self.script = len(ana_instance.script)
         self.links = ana_instance.links
         self.keys = jieba.analyse.extract_tags(self.text, num)
         self.words = list(jieba.cut(self.text))
@@ -21,4 +22,35 @@ class WebSta:
     def words_sta(self):
         return stat.w_stat(self.words, self.processer_num, words=self.keys)
 
+    @property
+    def title(self):
+        return { 'h' + str(i):len(self._res("h" + str(i))) for i in range(1,7)}
+
+    @property
+    def img(self):
+        return {'img': len(self._res("img"))}
+
+    @property
+    def content(self):
+        return {
+            i: len(self._res(i))
+            for i in ['ol', 'ul', 'li', 'div', 'span', 'p', 'i', 'b', 'br', 'img']
+            
+        }
+
+    @property
+    def meta(self):
+        return {
+            'meta': len(self._res("meta"))
+        }
+
+    @property
+    def statistic(self):
+        d = dict()
+        d.update(self.title)
+        d.update(self.content)
+        d.update(self.img)
+        d.update(self.meta)
+        d.update({'css': self.css, 'script': self.script})
+        return d
 
